757686571d9f55dbcec00755cb291be9b185bb0b
janet-do <124389477+janet-do@users.noreply.github.com>
2023-03-07 23:26:08 +0000

    allowed generation of hello world for js and fixed formatting

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 // Created by: Janet Do
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+//This generates hello world
 function myButtonClicked() {
   document.getElementById("hello-world").innerHTML = "Hello, World!"
 }
