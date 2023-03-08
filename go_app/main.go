b5d4260700eb34c389eaeb8c29af941ddc77e55c
janet-do <124389477+janet-do@users.noreply.github.com>
2023-03-08 17:55:32 +0000

    added go

---
+++ go_app/main.go
@@ -0,0 +1,16 @@
+// Created by: Janet Do
+// Created on: Sep 2020
+//
+// This program displays, "Hello, World!"
+
+package main
+
+import "fmt"
+
+func main() {
+    // This function displays, "Hello, World!"
+    // the Go standard is to indent using "tabs" NOT spaces!
+    fmt.Println("Hello, World!")
+    
+    fmt.Println("\nDone.")
+}
