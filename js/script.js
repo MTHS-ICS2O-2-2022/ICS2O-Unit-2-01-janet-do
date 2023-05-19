42fa0a7f194ce9a623f1f08c764711a2aaebd6e8
janet-do <124389477+janet-do@users.noreply.github.com>
2023-05-19 22:32:43 +0000

    sync

--- js/script.js
+++ js/script.js
@@ -4,6 +4,17 @@
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 //This generates hello world
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-2-01-janet-do/sw.js", {
+    scope: "/ICS2O-Unit-2-01-janet-do/",
+  })
+}
 function myButtonClicked() {
   document.getElementById("hello-world").innerHTML = "Hello, World!"
 }
